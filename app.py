b4b2c819e359bcea5f8581920bbeb29f41eb6df3
Gabriel-Lima0 <gabbriel.llima7@gmail.com>
2024-06-06 09:49:50 -0300

    alteçãoes completa. finalizado

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pyautogui
 # SENHAS
 pass_admin = "admin"
 pass_intelbras = "intelbras"
-pass_new = "********"
-ip = "********"
+pass_new = "0x6004VNF@"
+ip = "192.168.1.1"
 
 # IP NO NAVEGADOR
 pyperclip.copy(ip)
@@ -15,7 +15,6 @@ pyautogui.hotkey('ctrl', 'v')
 pyautogui.hotkey('enter')
 
 # USUARIO E SENHA
-
 pyperclip.copy(pass_admin)
 pyautogui.click(852, 381, duration=0.5)
 pyautogui.hotkey('ctrl', 'v')
@@ -26,7 +25,6 @@ pyautogui.hotkey('ctrl', 'v')
 pyautogui.hotkey('enter')
 
 # CONFIRMAR NOVA SENHA
-
 pyperclip.copy(pass_new)
 pyautogui.click(751, 481, duration=0.5)
 pyautogui.hotkey('ctrl', 'v')
@@ -43,8 +41,8 @@ pyautogui.hotkey('tab')
 
 pyautogui.hotkey('ctrl', 'v')
 
-pyautogui.click(706, 858, duration=0.3)
-pyautogui.click(950, 925, duration=0.5)
+pyautogui.click(706, 858, duration=0.2)
+pyautogui.click(950, 925, duration=0.1)
 
 # ACESSAR NOVAMENTE COM A NOVA SENHA
 #       usuario
@@ -62,17 +60,15 @@ pyautogui.hotkey('enter')
 pyautogui.click(852, 381, duration=0.5)
 
 # NEGAR ATUALIZACAO DE SENHA
-pyautogui.click(1444, 250, duration=0.5)
+pyautogui.click(1444, 250, duration=0.3)
 
 # ACESSAR CONFIGURACOES
-
-pyautogui.click(1249, 197, duration=0.8)
-pyautogui.click(606, 609, duration=0.8)
-pyautogui.click(816, 337, duration=0.5)
+pyautogui.click(1249, 197, duration=0.3)
+pyautogui.click(606, 609, duration=0.4)
+pyautogui.click(816, 337, duration=0.4)
 
 # ENVIAR ARQUIVO QUE ESTA NO COMPUTADOR
-
-pyautogui.click(564, 158, duration=0.7)
+pyautogui.click(564, 158, duration=0.5)
 pyautogui.hotkey('enter')
 
 pyautogui.click(762, 379, duration=0.5)
@@ -83,25 +79,25 @@ pyautogui.click(1000, 500, duration=0.5)
 # VALIDAR QUE BAIXOU
 
 # RECARREGAR A PAGINA
-pyautogui.click(1000, 500, duration=1)
-pyautogui.hotkey('ctrl', 'r')
-pyautogui.hotkey('ctrl', 'r')
+# pyautogui.click(1000, 500, duration=1)
+# pyautogui.hotkey('ctrl', 'r')
+# pyautogui.hotkey('ctrl', 'r')
 
 # ACESSAR ROTEADOR
 #        usuario
-pyperclip.copy(pass_admin)
-pyautogui.click(852, 381, duration=1)
-pyautogui.hotkey('ctrl', 'v')
-pyautogui.hotkey('tab')
+# pyperclip.copy(pass_admin)
+# pyautogui.click(852, 381, duration=1)
+# pyautogui.hotkey('ctrl', 'v')
+# pyautogui.hotkey('tab')
 
 #        senha
-pyperclip.copy(pass_new)
-pyautogui.hotkey('ctrl', 'v')
+# pyperclip.copy(pass_new)
+# pyautogui.hotkey('ctrl', 'v')
 
-pyautogui.hotkey('enter')
+# pyautogui.hotkey('enter')
 
-pyautogui.click(852, 381, duration=0.5)
+# pyautogui.click(852, 381, duration=0.5)
 
 # VALIDAR QUE ATUALIZOU
 
-pyautogui.click(1000, 500, duration=0.5)
+# pyautogui.click(1000, 500, duration=0.5)
